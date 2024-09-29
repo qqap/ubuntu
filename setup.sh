f2122ee05586b855f94ea42ea46c95bbfc07560b
qqap <lavahumour@gmail.com>
2024-09-29 15:05:08 -0700

    patch

--- setup.sh
+++ setup.sh
@@ -23,26 +23,6 @@ if id "a" &>/dev/null; then
     fi
 fi
 
-# Check if /home/a exists (in case the user was deleted but the directory remains)
-if [ -d "/home/a" ]; then
-    echo "Directory /home/a exists."
-    read -p "Are you sure you want to delete /home/a? (Type 'yes' to confirm): " confirm
-    if [ "$confirm" = "yes" ]; then
-        read -p "This action is irreversible. Type 'yes' again to confirm: " confirm2
-        if [ "$confirm2" = "yes" ]; then
-            echo "Deleting /home/a..."
-            sudo rm -rf /home/a
-        else
-            echo "Operation cancelled."
-            exit 1
-        fi
-    else
-        echo "Operation cancelled."
-        exit 1
-    fi
-fi
-
-
 # Create user "a"
 echo "Debug: Creating user 'a'"
 sudo useradd -m -s /bin/zsh a
@@ -58,23 +38,17 @@ sudo -u a mkdir -p /home/a/.ssh
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC4DhuwpxW2YuZBrEWuIFY0nrZsVIW4Kma7HMxeGL1F64fnK68plpRcojd0scjwXol7BC7Pv0PWTIvnDKWMrbSuVoOXjf5/s79UQ0RGT7zPOYg2onx1JytqSHnEM7x7/uPok9edZ0HwK16MZaH/p3EOpBzsB1hcIiFdAkKew7rmjy0a65hZpoi2ikciWYD8R40n7G1NuDgJBgwzCOShXSg3EqPN6R8dQ4olhJgQoZJnwpP7ZUJfrwdyJGa4aSc54tSWNMpWxlurEZiL/twCO2io0X8wjQ12tRHby4xP6Ejcnx9RLABrZdxFzHpow4ZhNI5aIxtv7PAi3W4WMOhzb9s6XNopBkkW851D5FIELiKSin3LY/6qts4LSbVretxek5L09FI9a6DkutAs9vjIJHkRUnr4exy7opjF3YprpgnlFPyvj9pznaKHvwtEuNxXX5qyoi6F3d6Zhc57XGcYXJbwHNelX6jNWJVBpmdPVZTwNr85BrZOR3gbcVTByDFa9kE= qqap@macpro.local" | sudo -u a tee /home/a/.ssh/authorized_keys
 sudo -u a chmod 600 /home/a/.ssh/authorized_keys
 
+
+
 # Switch to user "a" for the rest of the setup
 echo "Debug: Switching to user 'a'"
-sudo -u a bash << EOF
+sudo su - a
 
-# Create .zshrc file for user "a"
-touch ~/.zshrc
-
-# Setup for current user
+# setup for current user
 echo "Debug: Setting up zsh for current user"
 command -v zsh | sudo tee -a /etc/shells
 sudo chsh -s $(which zsh) $USER
 echo $SHELL
-
-# Continue with the rest of the script as user "a"
-EOF
-
-# Return to the original user to continue script execution
 
 # setup oh my zsh
 echo "Debug: Setting up Oh My Zsh"
